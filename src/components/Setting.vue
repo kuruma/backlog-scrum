22914ff6166f3cdc9832996786e7662d1c618144
Kuruma <mail@kuruman.org>
2018-04-04 23:35:11 +0900

    feat: Design setting forms

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="container" id="setting">
     <div class="row">
-      {{ msg }}
+      <div class="col">
+        <h1>設定</h1>
+        <form>
+          <div class="form-group row">
+            <label for="apikey" class="col-sm-3 col-md-2 col-form-label">APIキー</label>
+            <input type="text" class="col-sm-9 col-md-10 form-control" id="apikey"
+              placeholder="[個人設定] > [API] から新規に発行したAPIキーを入力"/>
+          </div>
+          <div class="form-group row">
+            <label for="spaceid" class="col-sm-3 col-md-2 col-form-label">スペースID</label>
+            <div class="input-group col-sm-9 col-md-10 pl-0 pr-0">
+              <div class="input-group-prepend">
+                <div class="input-group-text">https://</div>
+              </div>
+              <input type="text" class="form-control" id="spaceid" placeholder="ホスト部を入力"/>
+              <div class="input-group-postpend">
+                <div class="input-group-text">.{{ backlogDomain }}</div>
+              </div>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="domain" class="col-sm-3 col-md-2 col-form-label">接続先</label>
+            <select v-model="backlogDomain" class="col-sm-9 col-md-10 form-control" id="domain">
+              <option selected="selected">backlog.jp</option>
+              <option>backlog.com</option>
+            </select>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -11,26 +39,11 @@ export default {
   name: 'Setting',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      backlogDomain: 'backlog.jp',
     };
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
