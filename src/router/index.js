e49464fcf0610b665612cb5ac23f9f50ec5933e9
Kuruma <mail@kuruman.org>
2018-04-06 00:35:53 +0900

    style: Change page name from "setting" to "settings"

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import BootstrapVue from 'bootstrap-vue';
 import Storage from 'vue-web-storage';
-import Setting from '@/components/Setting';
+import Settings from '@/components/Settings';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -16,9 +16,9 @@ Vue.use(Storage, {
 export default new Router({
   routes: [
     {
-      path: '/setting',
-      name: 'Setting',
-      component: Setting,
+      path: '/settings',
+      name: 'Settings',
+      component: Settings,
     },
   ],
 });
