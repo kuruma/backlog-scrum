fb289b549288f887878dcb7971c9e0d76a956a65
Kuruma <mail@kuruman.org>
2018-04-06 22:01:45 +0900

    fix: unfixed past update to change path from setting to settings

--- test/unit/specs/Settings.spec.js
+++ test/unit/specs/Settings.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
-import Setting from '@/components/Setting';
+import Settings from '@/components/Settings';
 
 describe('Setting.vue', () => {
   xit('should render correct contents', () => {
-    const Constructor = Vue.extend(Setting);
+    const Constructor = Vue.extend(Settings);
     const vm = new Constructor().$mount();
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App');
