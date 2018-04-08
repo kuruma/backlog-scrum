f367829f03e049062f042216395216dfd1cce7b5
Kuruma <mail@kuruman.org>
2018-04-09 02:24:37 +0900

    style: Solve ugly setting forms in fluid mode

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -6,40 +6,48 @@
         <form>
           <div class="form-group row">
             <label for="apikey" class="col-sm-3 col-md-2 col-form-label">APIキー</label>
-            <input v-model="backlogApiKey"
-              type="text" class="col-sm-9 col-md-10 form-control" id="apikey"
-              placeholder="[個人設定] > [API] から新規に発行したAPIキーを入力"/>
+            <div class="col">
+              <input v-model="backlogApiKey"
+                type="text" class="form-control" id="apikey"
+                placeholder="[個人設定] > [API] から新規に発行したAPIキーを入力"/>
+            </div>
           </div>
           <div class="form-group row">
             <label for="spaceid" class="col-sm-3 col-md-2 col-form-label">スペースID</label>
-            <div class="input-group col-sm-9 col-md-10 pl-0 pr-0">
-              <div class="input-group-prepend">
-                <div class="input-group-text">https://</div>
-              </div>
-              <input v-model="backlogHostname"
-                type="text" class="form-control" id="spaceid" placeholder="ホスト部を入力"/>
-              <div class="input-group-postpend">
-                <div class="input-group-text">.{{ backlogDomain }}</div>
+            <div class="col">
+              <div class="input-group pl-0 pr-0">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">https://</div>
+                </div>
+                <input v-model="backlogHostname"
+                  type="text" class="form-control" id="spaceid" placeholder="ホスト部を入力"/>
+                <div class="input-group-postpend">
+                  <div class="input-group-text">.{{ backlogDomain }}</div>
+                </div>
               </div>
             </div>
           </div>
           <div class="form-group row">
             <label for="domain" class="col-sm-3 col-md-2 col-form-label">接続先</label>
-            <select v-model="backlogDomain"
-              class="col-sm-9 col-md-10 form-control" id="domain">
-              <option selected="selected">backlog.jp</option>
-              <option>backlog.com</option>
-            </select>
+            <div class="col">
+              <select v-model="backlogDomain"
+                class="form-control" id="domain">
+                <option selected="selected">backlog.jp</option>
+                <option>backlog.com</option>
+              </select>
+            </div>
           </div>
           <div class="form-group row">
             <label for="project" class="col-sm-3 col-md-2 col-form-label">プロジェクト</label>
-            <input v-model="backlogProjectKey"
-              type="text" class="col-sm-9 col-md-10 form-control" id="projectkey"
-              placeholder="プロジェクトキーかIDを入力"/>
+            <div class="col">
+              <input v-model="backlogProjectKey"
+                type="text" class="form-control" id="projectkey"
+                placeholder="プロジェクトキーかIDを入力"/>
+            </div>
           </div>
           <div class="form-group row">
-            <span class="col-sm-3 col-md-2 col-form-label">表示設定</span>
-            <div class="form-check col-sm-9 col-md-10 pt-2 pb-2">
+            <span class="col-sm-3 col-md-2 col-form-label mr-3">表示設定</span>
+            <div class="form-check col pt-2 pb-2">
               <input v-model="isFixedViewMode"
                 class="form-check-input" type="checkbox" value="" id="viewmode"/>
               <label class="form-check-label" for="viewmode">固定幅で表示する</label>
