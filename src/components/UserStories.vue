499f87a8d943804e19a2f18909d6b762b63c7f3f
Kuruma <mail@kuruman.org>
2018-04-16 00:23:22 +0900

    feat: Hide loading icon after datastore is ready

--- src/components/UserStories.vue
+++ src/components/UserStories.vue
@@ -1,19 +1,38 @@
 <template>
   <el-main v-loading="loading">
     <el-row>
-      <el-col>
+      <el-col :span="12">
+      </el-col>
+      <el-col :span="12">
       </el-col>
     </el-row>
   </el-main>
 </template>
 
 <script>
+import backlog from '@/utils/backlog';
+
 export default {
   name: 'UserStories',
   data() {
     return {
       loading: true,
     };
+  },
+  mixins: [
+    backlog,
+  ],
+  methods: {
+    applyDatastore() {
+      this.loading = false;
+    },
+  },
+  created() {
+    this.$on('datastore-updated', this.applyDatastore);
+    // datastore-updated only called this page is loaded at 1st time
+    if (this.projectKey) {
+      this.applyDatastore();
+    }
   },
 };
 </script>
